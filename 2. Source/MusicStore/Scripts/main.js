26419e1f14e3dee5de2874c1ce055ce9339fe4b8
carawam <carawam@gmail.com>
2014-08-14 17:05:49 +0700

    Album/Account Template

--- 2. Source/MusicStore/Scripts/main.js
+++ 2. Source/MusicStore/Scripts/main.js
@@ -3,4 +3,10 @@
         auto: true,
         autoControls: false
     });
+    $("select").addClass("form-control");
+    $("input[type=text]").addClass("form-control");
+    $("input[type=password]").addClass("form-control");
+    $("input[type=datetime]").addClass("form-control");
+    $("input[type=email]").addClass("form-control");
+    $("input[type=number]").addClass("form-control");
 });
